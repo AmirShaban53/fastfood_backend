53e072244bac5eaa50b06273b8efed0e4bd77650
unknown <amirshaban53@gmail.com>
2021-09-21 18:10:41 +0300

    feedback 2.4

--- models/db.js
+++ models/db.js
@@ -14,17 +14,18 @@ const testConfig = `postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSW
 
 const sequelize = new Sequelize(process.env.NODE_ENV === 'test'? testConfig : devConfig);
 
-
 const connectDB = async() => {
     try {
         await sequelize.authenticate();
         if(process.env.NODE_ENV !== 'test'){
             await sequelize.sync();
             logger.info("connection to database made!");
-
-        }else{
-            logger.info("connection to test database made!");
         }
+        else{
+            logger.info("connection to test database made!");
+
+        }
+        
 
     } catch (error) {
         logger.error("connection to database failed!", error);
