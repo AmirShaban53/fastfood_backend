10390f5382bbc3b41b28c23f42fdafee817b6f16
unknown <amirshaban53@gmail.com>
2021-09-21 18:51:10 +0300

    feedback 2.92

--- models/db.js
+++ models/db.js
@@ -6,7 +6,7 @@ const productionConfig= process.env.DATABASE_URL;
 
 const devConfig = `postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DEV_DATABASE}`;
 
-const testConfig = `postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_TEST_DATABASE}`;
+const testConfig = `postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@localhost:5432/postgres`;
     
     
 
